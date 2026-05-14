c117526822c943f9364e974e67971fd12163d30b
La The Phong <latp1102@gmail.com>
2026-05-14 16:17:19 +0700

    Session02_b4

---
+++ src/main/java/org/example/session02_b4/Session02B4Application.java
@@ -0,0 +1,13 @@
+package org.example.session02_b4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session02B4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session02B4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/session02_b4/Session02B4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.session02_b4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session02B4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
